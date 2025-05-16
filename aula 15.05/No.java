c5a7b0cd85fc1448513bf0fecdde3a75f82d9187
Fernando Mantovani <mantovani2909@gmail.com>
2025-05-15 22:02:45 -0300

    codigo corrigido

--- aula 15.05/No.java
+++ aula 15.05/No.java
@@ -1,8 +1,10 @@
 public class No {
     String valor;
-    No esquerdo, direito;
+    No esquerda, direita;
 
     public No(String valor) {
         this.valor = valor;
+        this.esquerda = null;
+        this.direita = null;
     }
 }
